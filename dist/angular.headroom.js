12e93d7d51c12f66486da4856ed7c753801e6004
Ronny Hartenstein <r.hartenstein@axilaris.de>
2015-06-01 17:06:20 +0200

    gen

--- dist/angular.headroom.js
+++ dist/angular.headroom.js
@@ -1,6 +1,6 @@
 /*!
  * headroom.js v0.7.0 - Give your page some headroom. Hide your header until you need it
- * Copyright (c) 2014 Nick Williams - http://wicky.nillia.ms/headroom.js
+ * Copyright (c) 2015 Nick Williams - http://wicky.nillia.ms/headroom.js
  * License: MIT
  */
 
@@ -19,6 +19,7 @@
       restrict: 'EA',
       scope: false,
       link: function(scope, element, attrs) {
+        console.log(attrs);
         var options = {};
         angular.forEach(Headroom.options, function(value, key) {
           options[key] = attrs[key] || Headroom.options[key];
@@ -46,6 +47,7 @@
             scope.$apply(attrs.onUnpin);
           };
         }
+        console.log(options);
         var headroom = new Headroom(element[0], options);
         headroom.init();
         scope.$on('$destroy', function() {
